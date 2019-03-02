340262aaf4adb512f85ae7eea4126538001da57d
rohitb <rbaweja2311@gmail.com>
2019-03-02 12:42:11 +0530

    My First Commit

---
+++ src/git1.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class git1 {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("Will work with code/Users/rohitbaweja/Desktop/chromedriver");
+		System.setProperty("webdriver.chrome.driver","/Users/rohitbaweja/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+	
+	
+}
